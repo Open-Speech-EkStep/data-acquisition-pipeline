5c28e81638c5a2c0e80620d85846fc82b9f332a4
Aman Tiwari <amant5186@gmail.com>
2020-08-05 11:46:04 +0530

    140 | Aman & Soujyo | Reformatting the configurations

--- packages/data_acquisition_framework/data_acquisition_framework/pipeline_config.py
+++ packages/data_acquisition_framework/data_acquisition_framework/pipeline_config.py
@@ -1,12 +1,21 @@
+
 mode = 'file'  # [channel,file]
+
+
+#Common configurations
 bucket = 'ekstepspeechrecognition-dev'
+channel_blob_path = 'data/audiotospeech/raw/download/downloaded/hindi/audio'
+archive_blob_path = 'archive'
+source_name = 'DD_Jharkhand'  #Scraped Data file name(CSV)
+batch_num = 5 # keep batch small on free tier
+scraped_data_blob_path = "scraped_data"
+
+#Channel mode configurations
 channel_url = 'https://www.youtube.com/channel/UCIWcrT3URyL9HDyGgSJfnpg/playlists'
 match_title_string = ''
 reject_title_string = ''
-channel_blob_path = 'data/audiotospeech/raw/download/downloaded/hindi/audio'
-archive_blob_path = 'archive'
-source_name = 'DD_Jharkhand'  # Scraped Data file path(CSV)
-batch_num = 5 # keep batch small on free tier
-scraped_data_blob_path = "scraped_data"
+
+
+#File Mode configurations
 file_speaker_name_column = "speaker_name"
 file_url_name_column = "video_url"
