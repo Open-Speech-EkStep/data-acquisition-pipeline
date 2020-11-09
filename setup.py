d14769adfc12dfd1fc47ce1b31a49e28048dc754
Sreejith V <sreejith.v@thoughtworks.com>
2020-11-09 15:57:50 +0530

    fix(scrapinghub): add package data to read json in scrapinghub

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     name         = 'project',
     version      = '1.0',
     packages     = find_packages(),
+    package_data = {'data_acquisition_framework': ['configs/config.json']},
     entry_points = {'scrapy': ['settings = data_acquisition_framework.settings']},
 )
